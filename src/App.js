4b205ee17aa0d03f17f1214454a18fb72f758744
Kyle Behiels <kyle@kyle-XPS-15.hitronhub.home>
2018-10-30 18:40:21 -0700

    Login format done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
+import '../node_modules/jquery/dist/jquery';
+import '../node_modules/bootstrap/dist/js/bootstrap';
+import '../node_modules/react-popper/dist/index.umd';
 import logo from './logo.svg';
 import './App.css';
 import AppRouter from './components/AppRouter';
+import LoginPage from './pages/LoginPage';
 
-class App extends Component {
+const loggedIn = false;
+
+class App extends Component { 
+
+  constructor(){
+    super();
+    this.loginCheck = this.loginCheck.bind(this);
+  }
+
+  loginCheck(){
+    if(loggedIn){
+      return (
+        <div className="App">
+          <header className="App-header">
+          <link rel="stylesheet" href="./App.css"></link>
+          </header>
+          <div className="container">
+            <AppRouter></AppRouter>
+          </div>
+        </div>
+      );
+    }
+    else {
+      return (
+        <LoginPage></LoginPage>
+      );
+    }
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <link rel="stylesheet" href="./App.css"></link>
-        </header>
-        <div className="container">
-          <AppRouter></AppRouter>
-        </div>
+      <div>
+        {this.loginCheck()}
       </div>
     );
   }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+
+class LoginPage extends Component{
+    constructor(props){
+        super(props);
+        this.toggleSignup = this.toggleSignup.bind(this);
+        this.state = {
+            signup : false
+        }
+    }
+
+    toggleSignup(){
+        let signupForm = document.getElementById('signup_form');
+        if(this.state.signup){
+            signupForm.classList.add("hidden");
+            this.setState({
+                signup: false
+            });
+        }
+        else{
+            signupForm.classList.remove("hidden");
+            this.setState({
+                signup: true
+            });
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="container">
+                    <div className="header text-center">
+                        <h1>Landlord application</h1>
+                    </div>
+                    
+                    <hr></hr>
+
+                    <div id="login_card" className="card">
+                        <div className="card-body">
+                            <h2>Sign In</h2>
+                            <hr></hr>
+                            <form>
+                                <div className="form-group">
+                                    <input className="form-control" type="text" placeholder="Email" ></input>
+                                    <br></br>
+                                    <input className="form-control" type="password" placeholder="Password"></input>
+                                </div>
+                                <input className="form-control btn btn-primary" type="submit" value="Submit"></input>
+                            </form>
+                            <hr></hr>
+                            <button id="signup_button" className="btn btn-secondary" onClick={this.toggleSignup}>Don't have an account?</button>
+                            <form id="signup_form" className="hidden">
+                                <div className="form-group">
+                                    <input className="form-control" type="text" placeholder="Email"></input>
+                                    <br></br>
+                                    <input className="form-control" type="password" placeholder="Password"></input>
+                                    <br></br>
+                                    <input className="form-control" type="password" placeholder="Confirm"></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
