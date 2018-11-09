6c98a3f35b255fa3f74974d30e7b93a0a755a40e
Andrew Adamczyk <aadamczyk@tru.ca>
2018-11-09 14:01:29 -0800

    test commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class App extends Component {
-
+//test
 
     constructor(){
     super();
