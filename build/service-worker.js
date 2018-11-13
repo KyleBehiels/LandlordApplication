513f0a1b9d20a8c3ebacd443f4a75faa943cb8ab
Kyle Behiels <kylejamesbehiels@gmail.com>
2018-11-12 22:07:16 -0800

    Final

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9da1cc2debac03e2e19f18f9590d264f.js"
+  "./Landlord/precache-manifest.bd4304a6f750d52feeb1e4befbe31afa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("./Landlord/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
